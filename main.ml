fc5991e40e030fcb16a9b5f439bee6842923bec7
Tom Plick <tomplick@gmail.com>
2023-10-12 22:38:37 -0400

    accept cmd line args

--- main.ml
+++ main.ml
@@ -11,14 +11,34 @@ let analyze_deal deal counter idx =
     in (* Printf.printf "gamma depth %d: value %d, nodes %d\n%!" 52 value node_count; *)
        counter := !counter + node_count
 
-let _ =
+let run_benchmark limit =
     let counter = ref 0 in
-    (* for i = 1 to 20 do *)
-    for i = 1 to 20 do
+    for i = 1 to limit do
         Printf.printf "#%d\n" i;
         let d = new_deal ()
         in analyze_deal d counter i;
         Printf.printf "\n"
     done;
     Printf.printf "Saw %d nodes.\n" !counter
+
+let run_single idx =
+    let counter = ref 0 in
+    for i = 1 to idx do
+        let d = new_deal ()
+        in if i = idx then
+            (Printf.printf "#%d\n" i;
+             analyze_deal d counter i;
+             Printf.printf "\n")
+    done;
+    Printf.printf "Saw %d nodes.\n" !counter
+
+let _ =
+    let arg_queue = ref (List.tl @@ Array.to_list Sys.argv) in
+    let next_arg () = (let arg = List.hd !arg_queue in arg_queue := List.tl !arg_queue; arg) in
+    match next_arg () with
+        | "-bench" -> let limit = int_of_string (next_arg ())
+                      in run_benchmark limit
+        | "-single" -> let idx = int_of_string (next_arg ())
+                      in run_single idx
+        | _ -> Printf.printf "Bad arguments.\n"
 
